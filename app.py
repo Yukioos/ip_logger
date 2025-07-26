b53791742ca3253f0a013d3868a03c41779c3519
yukio <yukio.ogh@.com>
2025-07-27 01:32:53 +0300

    update geo

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 BOT_TOKEN = '8439041030:AAFK7c9-xia3_fWtY4mMzorWfKuVWgRSnf4'
 CHAT_ID = '774044847'
 
-def send_telegram_message(ip):
+def send_telegram_message(ip, geo_info):
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-    data = {'chat_id': CHAT_ID, 'text': f'Новый посетитель сайта с IP: {ip}'}
+    data = {'chat_id': CHAT_ID, 'text': f'Новый посетитель сайта с IP: {ip} {geo_info}'}
     requests.post(url, data=data)
 
 @app.route('/')
@@ -18,10 +18,24 @@ def index():
         ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.remote_addr
-
+    geo_info = get_geo_info(ip)
     send_telegram_message(ip)
 
     return render_template('index.html')
+
+def get_geo_info(ip):
+    try:
+        response = requests.get(f'http://ip-api.com/json/{ip}')
+        data = response.json()
+        if data['status'] == 'success':
+            country = data.get('country', 'Unknown')
+            city = data.get('city', 'Unknown')
+            return f'{country}, {city}'
+        else:
+            return 'Geo info not available'
+    except:
+        return 'Geo lookup failed'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
