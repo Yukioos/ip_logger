0a84a0e2f0c40d5742d33d5e0d9b956a3cb40b6e
yukio <yukio.ogh@.com>
2025-07-27 00:47:08 +0300

    заливание айпи в йафл

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+IP_LOG_FILE = 'ips.txt'
+
+
+def save_ip(ip):
+    with open(IP_LOG_FILE, 'a') as f:
+        f.write(ip + '\n')
+
+
 @app.route('/')
 def index():
     x_forwarded_for = request.headers.get('X-Forwarded-For')
     if x_forwarded_for:
-        # Берём первый IP из списка, если прокси несколько
         ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.remote_addr
+
+    save_ip(ip)
     return f'Ваш IP: {ip}'
 
-@app.route('/headers')
-def headers():
-    headers = dict(request.headers)
-    # Красиво выводим все заголовки в теле ответа
-    return "<pre>" + "\n".join(f"{k}: {v}" for k, v in headers.items()) + "</pre>"
 
 if __name__ == '__main__':
     app.run(debug=True)
